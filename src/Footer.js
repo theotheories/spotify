53ed042258eb3af17d87e13c1c225e31b1c81f3d
theotheories <57911269+theotheories@users.noreply.github.com>
2021-03-23 12:16:14 +0000

    footer ui looking good, non functional, adding to body now

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,21 @@ import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Album and song details</p>
+                <img className="footer__albumLogo" 
+                src="https://upload.wikimedia.org/wikipedia/en/2/2e/Usher-yeah.jpg" alt=""/>
+                <div className="footer__songInfo">
+                <h4>Yeah!</h4>
+                <p>Usher</p>
+                </div>
             </div>
             <div className="footer__centre">
                 <ShuffleIcon className="footer__green" />
@@ -20,7 +29,17 @@ function Footer() {
                 <RepeatIcon className="footer__green" />
             </div>
             <div className="footer__right">
-                <p>Volume controls</p>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
         </div>
     )
